267fc05c156ac11e4523eee29240e6218052965c
Jeroen Stobbe <90630068+stobbej@users.noreply.github.com>
2021-12-14 15:20:17 +0100

    Create textmodel.py
    Direct overgenomen vanuit de projectbeschrijving, zie kopje De klasse TextModel op pagina https://misja.github.io/programmeren/projects/textid.html

---
+++ textmodel.py
@@ -0,0 +1,43 @@
+# Fall 2021 - Leergang Programmeren TextID
+# textmodel.py
+#
+# Opdracht: Tekstidentificatie
+#
+# Naam: Marlies Wanders, Jeroen van Kleef, Jeroen Stobbe
+#
+
+
+class TextModel:
+    """A class supporting complex models of text."""
+
+    def __init__(self):
+        """Create an empty TextModel."""
+        #
+        # Maak dictionary's voor elke eigenschap
+        #
+        self.words = {}             # Om woorden te tellen
+        self.word_lengths = {}      # Om woordlengtes te tellen
+        self.stems = {}             # Om stammen te tellen
+        self.sentence_lengths = {}  # Om zinslengtes te tellen
+        #
+        # Maak een eigen dictionary
+        #
+        self.my_feature = {}        # Om ... te tellen
+
+    def __repr__(self):
+        """Display the contents of a TextModel."""
+        s = 'Woorden:\n' + str(self.words) + '\n\n'
+        s += 'Woordlengtes:\n' + str(self.word_lengths) + '\n\n'
+        s += 'Stammen:\n' + str(self.stems) + '\n\n'
+        s += 'Zinslengtes:\n' + str(self.sentence_lengths) + '\n\n'
+        s += 'MIJN EIGENSCHAP:\n' + str(self.my_feature)
+        return s
+
+    # Voeg hier andere methodes toe.
+    # Je hebt in het bijzonder methodes nodig die het model vullen.
+
+
+# Hier kan je dingen testen...
+tm = TextModel()
+# Zet hier aanroepen neer die het model vullen met informatie
+print('TextModel:', tm)
